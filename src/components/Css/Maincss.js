94d6f00371de380d2708cb89a4e9d9315ff535db
Shripati Avasthi <shripatiavasthi@Shripatis-MacBook-Pro.local>
2022-08-29 00:22:15 +0530

    weather app

---
+++ src/components/Css/Maincss.js
@@ -0,0 +1,9 @@
+import { StyleSheet , Dimensions } from "react-native";
+
+const { height, width } = Dimensions.get('screen');
+
+export const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'red',
+    }
+});
