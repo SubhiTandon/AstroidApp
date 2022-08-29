0cfe0e9a11b61eaedd97fb7ce19cd19d20b0f733
Shripati Avasthi <shripatiavasthi@Shripatis-MacBook-Pro.local>
2022-08-29 18:43:52 +0530

    Astroid app

--- src/components/Hoome/Asteroiddetails.tsx
+++ src/components/Hoome/Asteroiddetails.tsx
@@ -1,103 +1,128 @@
-import React, { useState } from 'react'
-import { View, Text, SafeAreaView, Alert, StyleSheet, Image, Button, ScrollView } from 'react-native'
-import axios from "axios";
+// import React, { useState } from 'react'
+// import { View, Text, SafeAreaView, Alert, StyleSheet, Image, Button, ScrollView } from 'react-native'
+// import axios from "axios";
 
-type Props = {
+// type Props = {
 
-}
+// }
 
+
+// const Asteroiddetails = (props: Props) => {
+
+
+//   const [countrydetails, setcountrydetails] = useState()
+
+//   const weather = (data: string) => {
+//     // Alert.alert("function call.");
+//     axios.get(`http://api.weatherstack.com/current?access_key=d68383d3cb470ad44cbee15ef6a94864&query=${data}`)
+//       .then((response) => {
+//         console.log(response?.data, "weather api response");
+//         setcountrydetails(response.data)
+//       })
+//       .catch((error) => {
+//         console.log(error, "weather api error");
+//         Alert.alert("api error.", JSON.stringify(error));
+//       });
+//   }
+
+
+
+//   return (
+//     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+//       <ScrollView>
+//         {props?.route?.params && props?.route?.params.map((item: any, index: number) => {
+//           return (
+//             <>
+//               <View style={styles.card} key={index}>
+//                 <View style={styles.row}>
+//                   <Text >capital : {item.capital} </Text>
+//                 </View>
+//                 <View style={styles.row}>
+//                   <Text>population : {item.population}</Text>
+//                 </View>
+//                 <View style={styles.row}>
+//                   <Text>Lat/Long {item.latlng[0]} {item.latlng[1]} </Text>
+//                 </View>
+//                 <View style={styles.row}>
+//                   <Text>flag  </Text>
+//                   <Image source={{ uri: `${item.flags.png}` }} style={styles.image} />
+//                 </View>
+//                 <View >
+//                   <Button
+//                     title="GET CAPITAL WEATHER"
+//                     accessibilityLabel="decrement"
+//                     onPress={() => {
+//                       weather(item.capital[0])
+//                     }}
+//                     color="blue"
+//                   />
+//                 </View>
+//               </View>
+//             </>
+//           )
+//         })}
+//         {countrydetails ?
+//           <View style={styles.card}>
+//             <View style={styles.row}>
+//               <Text>Temperature                :                   {countrydetails?.current?.temperature}</Text>
+//             </View>
+//             <View style={styles.row}>
+//               <Text>Weather Icon                :                   <Image source={{ uri: `${countrydetails?.current?.weather_icons[0]}` }} style={styles.image} /></Text>
+//             </View>
+//             <View style={styles.row}>
+//               <Text>wind_speed                 :                   {countrydetails?.current?.wind_speed}</Text>
+//             </View>
+//             <View style={styles.row}>
+//               <Text>precipitation                :                   {countrydetails?.current?.precip}</Text>
+//             </View>
+//           </View> : null}
+//       </ScrollView>
+//     </SafeAreaView>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   card: {
+//     margin: 10,
+//     // backgroundColor: 'pink',
+//     padding: 10
+//   },
+//   row: {
+//     margin: 10,
+//     padding: 5,
+//     borderBottomColor: 'black',
+//     borderBottomWidth: 1,
+//     flexDirection: 'row',
+//   },
+//   image: {
+//     height: 30,
+//     width: 30,
+//   }
+// })
+
+// export default Asteroiddetails
+
+import React from 'react'
+import { View , Text , SafeAreaView } from 'react-native'
+
+type Props = {}
 
 const Asteroiddetails = (props: Props) => {
 
+  console.log(props?.route?.params?.nasa_jpl_url , "props in astrooid") 
 
-  const [countrydetails, setcountrydetails] = useState()
-
-  const weather = (data: string) => {
-    // Alert.alert("function call.");
-    axios.get(`http://api.weatherstack.com/current?access_key=d68383d3cb470ad44cbee15ef6a94864&query=${data}`)
-      .then((response) => {
-        console.log(response?.data, "weather api response");
-        setcountrydetails(response.data)
-      })
-      .catch((error) => {
-        console.log(error, "weather api error");
-        Alert.alert("api error.", JSON.stringify(error));
-      });
-  }
-
-
+  
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <ScrollView>
-        {props?.route?.params && props?.route?.params.map((item: any, index: number) => {
-          return (
-            <>
-              <View style={styles.card} key={index}>
-                <View style={styles.row}>
-                  <Text >capital : {item.capital} </Text>
-                </View>
-                <View style={styles.row}>
-                  <Text>population : {item.population}</Text>
-                </View>
-                <View style={styles.row}>
-                  <Text>Lat/Long {item.latlng[0]} {item.latlng[1]} </Text>
-                </View>
-                <View style={styles.row}>
-                  <Text>flag  </Text>
-                  <Image source={{ uri: `${item.flags.png}` }} style={styles.image} />
-                </View>
-                <View >
-                  <Button
-                    title="GET CAPITAL WEATHER"
-                    accessibilityLabel="decrement"
-                    onPress={() => {
-                      weather(item.capital[0])
-                    }}
-                    color="blue"
-                  />
-                </View>
-              </View>
-            </>
-          )
-        })}
-        {countrydetails ?
-          <View style={styles.card}>
-            <View style={styles.row}>
-              <Text>Temperature                :                   {countrydetails?.current?.temperature}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text>Weather Icon                :                   <Image source={{ uri: `${countrydetails?.current?.weather_icons[0]}` }} style={styles.image} /></Text>
-            </View>
-            <View style={styles.row}>
-              <Text>wind_speed                 :                   {countrydetails?.current?.wind_speed}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text>precipitation                :                   {countrydetails?.current?.precip}</Text>
-            </View>
-          </View> : null}
-      </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+    <View>
+        <Text>Astroid details</Text>
+        <Text>name : - {props?.route?.name}</Text>
+        <Text>nasa_jpl_url : - {props?.route?.params?.nasa_jpl_url}</Text>
+        <Text>is_potentially_hazardous_asteroid : - {props?.route?.params?.is_potentially_hazardous_asteroid  == true ? "true" : "false"}</Text>
+    </View>
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  card: {
-    margin: 10,
-    // backgroundColor: 'pink',
-    padding: 10
-  },
-  row: {
-    margin: 10,
-    padding: 5,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-  },
-  image: {
-    height: 30,
-    width: 30,
-  }
-})
 
 export default Asteroiddetails
--- src/components/Hoome/Home.tsx
+++ src/components/Hoome/Home.tsx
@@ -1,4 +1,71 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { Button, StyleSheet, Text, View, TextInput, Alert, Image, Dimensions, Modal, Pressable } from 'react-native';
+// import axios from "axios";
+
+// const { height, width } = Dimensions.get('screen');
+
+// export type Props = {
+//     city: string;
+//     ListRender: boolean;
+//     modal: boolean;
+//     capital: any;
+// };
+
+// const Hello: React.FC<Props> = ({ city, ListRender, capital, modal, navigation }) => {
+
+//     const [asteroiddetails, setasteroiddetails] = useState({})
+//     const [cityname, setcityname] = useState(city)
+//     const [ListshouldRender, setListshouldRender] = useState(ListRender)
+//     const [citydetails, setcitydetails] = useState([])
+
+//     const submitbutton = () => {
+//         axios.get(`https://restcountries.com/v3.1/name/${cityname}`)
+//             .then(function (response) {
+//                 if (response.status === 404) {
+//                     setListshouldRender(false)
+//                 } else if (response.status === 200) {
+//                     setListshouldRender(true)
+//                     console.log(response.data , " weather api")
+//                     navigation.navigate("Asteroiddetails", response.data)
+//                 }
+//             })
+//             .catch(function (error) {
+//                 console.log(error, "weather api error");
+//             });
+//     }
+
+
+//     return (
+//         <View style={styles.container}>
+//             <TextInput placeholder='Country name' onChangeText={(value) => {
+//                 setcityname(value);
+//             }}></TextInput>
+//             <Button
+//                 disabled={cityname ? false : true}
+//                 title="SEARCH"
+//                 accessibilityLabel="decrement"
+//                 onPress={() => {
+//                     submitbutton()
+//                 }}
+//                 color="blue"
+//             />
+//         </View>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center'
+//     },
+// });
+
+// export default Hello;
+
+
+
+import React, { useEffect , useState }  from 'react';
 import { Button, StyleSheet, Text, View, TextInput, Alert, Image, Dimensions, Modal, Pressable } from 'react-native';
 import axios from "axios";
 
@@ -11,44 +78,74 @@ export type Props = {
     capital: any;
 };
 
-const Hello: React.FC<Props> = ({ city, ListRender, capital, modal, navigation }) => {
+const Hello: React.FC<Props> = ({ city, ListRender, capital, modal , navigation }) => {
 
-    const [asteroiddetails, setasteroiddetails] = useState({})
-    const [cityname, setcityname] = useState(city)
-    const [ListshouldRender, setListshouldRender] = useState(ListRender)
-    const [citydetails, setcitydetails] = useState([])
+    const [astroidid, setastroidid] = useState(city)
+    const [showlogic, setshowlogic] = useState(false)
+    const [asteroiddetails , setasteroiddetails] = useState({})
 
-    const submitbutton = () => {
-        axios.get(`https://restcountries.com/v3.1/name/${cityname}`)
+    const submitbutton = (data : any) => {
+        axios.get(`https://api.nasa.gov/neo/rest/v1/neo/${data}?api_key=BOGCU1F4Qm8pYhyWdVJscsT0PC8RnbyEc9gr2RuL`)
             .then(function (response) {
-                if (response.status === 404) {
-                    setListshouldRender(false)
-                } else if (response.status === 200) {
-                    setListshouldRender(true)
-                    console.log(response.data , " weather api")
-                    navigation.navigate("Asteroiddetails", response.data)
-                }
+                console.log(response.data , "??????????") 
+                setasteroiddetails(response.data)
+                navigation.navigate("Asteroiddetails" , response.data )
             })
             .catch(function (error) {
                 console.log(error, "weather api error");
             });
     }
 
+    const RandomAstroid = (data: string) => {
+        // Alert.alert("function call.");
+        axios.get(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`)
+            .then((response) => {
+                var astro = response?.data?.near_earth_objects
+                var randAst = astro[Math.floor(Math.random() * astro.length)];
+                console.log(randAst?.id , "api astroid response")
+                submitbutton(randAst?.id)
+            })
+            .catch( (error) => {
+                console.log(error, "asteroid error");
+            });
+    }
+
 
     return (
         <View style={styles.container}>
-            <TextInput placeholder='Country name' onChangeText={(value) => {
-                setcityname(value);
-            }}></TextInput>
-            <Button
-                disabled={cityname ? false : true}
-                title="SEARCH"
-                accessibilityLabel="decrement"
-                onPress={() => {
-                    submitbutton()
-                }}
-                color="blue"
-            />
+            <View >
+                <Text style={styles.greeting}>
+                    Welcome to Weather App
+                </Text>
+                <TextInput placeholder='Enter Astroid iD' onChangeText={(value) => {
+                    setastroidid(value);
+                    if (value.length > 0) {
+                        setshowlogic(false)
+                    } else {
+                        setshowlogic(true)
+                    }
+                }}></TextInput>
+                <View>
+                    <Button
+                        disabled={showlogic}
+                        title="Submit"
+                        accessibilityLabel="decrement"
+                        onPress={() => { 
+                            submitbutton(astroidid)
+                        }}
+                        color="red"
+                    />
+                </View>
+
+                <View style={{ marginTop: 10}}>
+                    <Button
+                        title="Random Astroid"
+                        accessibilityLabel="decrement"
+                        onPress={() => { RandomAstroid()}}
+                        color="red"
+                    />
+                </View>
+            </View>
         </View>
     );
 };
@@ -57,8 +154,77 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        // justifyContent: 'center'
     },
+    greeting: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 16
+    },
+    list: {
+        height: height / 1,
+        width: width / 1.1,
+        // backgroundColor: 'pink',
+        marginTop: 10,
+    },
+    card: {
+        margin: 10,
+        backgroundColor: 'pink',
+        flexDirection: 'row',
+        padding: 10
+    },
+    subview: {
+        margin: 10,
+    },
+    button: {
+        margin: 10,
+    },
+    image: {
+        height: 130,
+        width: 120,
+    },
+
+    /// modal design
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        // marginTop: 20,
+        backgroundColor: '#eae9ee',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        flexDirection: 'row',
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    }
+
+
 });
 
 export default Hello;
